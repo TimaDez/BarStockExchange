015df8b48320c5a7b3804bf3dc9cb23930add320
Tim <30781511+TimaDez@users.noreply.github.com>
2026-01-06 22:46:12 +0200

    Miggration files updated

---
+++ src/OrdersApi/Migrations/20260106195526_AddOutboxBackoffFields.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrdersApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOutboxBackoffFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "DeadLetteredAtUtc",
+                table: "OutboxMessages",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "NextAttemptAtUtc",
+                table: "OutboxMessages",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeadLetteredAtUtc",
+                table: "OutboxMessages");
+
+            migrationBuilder.DropColumn(
+                name: "NextAttemptAtUtc",
+                table: "OutboxMessages");
+        }
+    }
+}
